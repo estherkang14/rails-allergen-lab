b14a686416a7c804907d756325e343857bb4f125
estherkang14 <estherkang14@gmail.com>
2020-06-29 11:19:52 -0400

    created models + model relationships

---
+++ allergen/db/migrate/20200629151221_create_allergens_table.rb
@@ -0,0 +1,8 @@
+class CreateAllergensTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :allergens do |t|
+      t.integer :user_id
+      t.integer :ingredient_id
+    end
+  end
+end
