159b1f2916eebfedc6a9dcdd8a7e253e31d22ce8
estherkang14 <estherkang14@gmail.com>
2020-06-29 18:01:07 -0400

    minor changes, couldn't figure out index though

--- allergen/app/controllers/ingredients_controller.rb
+++ allergen/app/controllers/ingredients_controller.rb
@@ -1,10 +1,44 @@
 class IngredientsController < ApplicationController
+    before_action :find_ingredient, only: [:show, :destroy, :edit, :update]
+
     def index 
+        #@ingredients = Ingredient.all
+        @ingredients = Ingredient.sorted_ingredients
     end 
 
     def show
+    end 
+
+    def new
+        @ingredient = Ingredient.new
+    end 
+
+    def create
+        @ingredient = Ingredient.create(ing_params)
+        redirect_to ingredient_path(@ingredient.id)
+    end 
+
+    def edit
+    end
+
+    def update
+        @ingredient.update(ing_params)
+        redirect_to ingredient_path(@ingredient.id)
+    end 
+
+    def destroy
+        @ingredient.destroy
+        redirect_to ingredients_path
+    end 
+
+    private 
+    
+    def find_ingredient
         @ingredient = Ingredient.find(params[:id])
     end 
 
+    def ing_params
+        params.require(:ingredient).permit(:name) 
+    end 
     
 end
--- allergen/app/models/ingredient.rb
+++ allergen/app/models/ingredient.rb
@@ -3,4 +3,13 @@ class Ingredient < ApplicationRecord
     has_many :recipes, through: :recipe_ingredients
     has_many :allergies 
     has_many :users, through: :allergies
+
+    def allergy_count
+        self.users.count 
+    end 
+
+    def self.sorted_ingredients
+        ings = Ingredient.select('ingredient.id', 'ingredient.name')
+        byebug
+    end 
 end
